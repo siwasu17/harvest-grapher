178fa91cf0270321c8decf6de0d49bdbd5233667
siwasu17 <siwasu17@gmail.com>
2013-03-10 18:47:58 +0900

    Sinatra app mode

--- okkake.rb
+++ okkake.rb
@@ -1,8 +1,24 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader'
+require 'redis'
+require 'haml'
 
-get '/' do
-  "Hello World!"
+class Okkake < Sinatra::Base
+	enable :inline_templates
+	enable :logging
+
+	# Sinatra再起動が不要になる(gem入れたときは別)
+	set :server, "webrick"
+	register Sinatra::Reloader
+
+	get '/' do
+		"Hello world, it's #{Time.now} at the server!"
+	end
+
+	get '/:id' do
+	    @id = params[:id]
+		haml :index
+	end
 end
-
