40359691d297e7f2f203086a2c06037473fcf72c
siwasu17 <siwasu17@gmail.com>
2013-03-11 01:31:57 +0900

    initial version

--- okkake.rb
+++ okkake.rb
@@ -1,26 +1,42 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 require 'sinatra/base'
-require 'sinatra/reloader'
 require 'redis'
+require 'yaml'
 require 'haml'
 
 class Okkake < Sinatra::Base
 	enable :inline_templates
 	enable :logging
 
-	# Sinatra再起動が不要になる(gem入れたときは別)
-	# herokuへ入れるときはコメントアウトすること
-	set :server, "webrick"
-	register Sinatra::Reloader
+  configure do
+    require 'sinatra/reloader'
+    set :server, "webrick"
+    register Sinatra::Reloader
+  end
 
-	get '/' do
-		"Hello world, it's #{Time.now} at the server!"
+	get '/' do  
+	  @config = YAML.load_file(File.expand_path(File.dirname(__FILE__) + '/redis-config.yml'))
+    @now_time = Time.now.to_i
+	  redis = Redis.new(
+      :host=>@config['HOST'],
+      :port=>@config['PORT'],
+      :password=>@config['PASSWORD'])
+    @dot = {}
+    @show_count = 10
+    @span = 5
+    # 過去データをRedisから読み込み
+    ((@now_time - (@span*@show_count))..@now_time).step(@span).each do |t|
+      val = redis.get(t)
+      val = "0" if val.nil?
+      @dot[t] = val.to_f
+    end
+
 		haml :top
 	end
 
 	get '/:id' do
-	    @id = params[:id]
+	  @id = params[:id]
 		haml :index
 	end
 end
